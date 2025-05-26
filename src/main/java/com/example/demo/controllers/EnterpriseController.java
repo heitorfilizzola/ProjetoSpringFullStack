8c63d24595baf26d39741aab0412c3b2fdea6e30
Heitor Filizzola <76500311+heitorfilizzola@users.noreply.github.com>
2025-05-25 21:01:46 -0300

    feat: Páginas de criação de empresa, usuário e de mostrar a empresa

--- src/main/java/com/example/demo/controllers/EnterpriseController.java
+++ src/main/java/com/example/demo/controllers/EnterpriseController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dto.CreateEmployeeDTO;
 import com.example.demo.dto.CreateEnterpriseDTO;
 import com.example.demo.dto.RegisterRequestDTO;
 import org.springframework.stereotype.Controller;
@@ -9,10 +10,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class EnterpriseController {
 
+    @GetMapping("Enterprise")
+    public ModelAndView showEnterprise() {
+        ModelAndView mv = new ModelAndView("Enterprise_Page/index.html");
+        return mv;
+    }
+
     @GetMapping("createEnterprise")
     public ModelAndView createEnterprise() {
         ModelAndView mv = new ModelAndView("CreateEnterprise_Page/index.html");
         mv.addObject("createEnterpriseDTO", new CreateEnterpriseDTO());
+        return mv;
+    }
+
+    @GetMapping("createEmployee")
+    public ModelAndView createEmployee() {
+        ModelAndView mv = new ModelAndView("CreateEmployee_Page/index.html");
+        mv.addObject("createEmployeeDTO", new CreateEmployeeDTO());
         return mv;
     }
 
